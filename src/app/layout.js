84d046da097f948fd269068e9d08b7a0d3453bfc
Rithish-Abinav-Developer <your-github-email@example.com>
2026-07-19 21:10:40 +0530

    add

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,11 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import { Lexend_Deca } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const lexendDeca = Lexend_Deca({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-lexend-deca",
 });
 
 export const metadata = {
@@ -18,7 +15,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en"  className={lexendDeca.className}>
       <body>{children}</body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,66 +1,63 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import React from 'react'
 
-export default function Home() {
+export default function page() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    <>
+    
+    <form autoComplete="off">
+      <input
+        type="text"
+        placeholder="Name"
+        autoComplete="off"
+      />
+
+      <input
+        type="email"
+        placeholder="Email"
+        autoComplete="off"
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        autoComplete="new-password"
+      />
+
+      <div className='gender'>
+        <p>Select Gender</p>
+     <div className="radio_select">
+      <div>
+        <span>Male</span>
+  <input
+    type="radio"
+    id="male"
+    name="gender"
+    value="male"
+    hidden
+  />
+  <label htmlFor="male"></label>
+</div>
+
+<div>
+        <span>Female</span>
+  <input
+    type="radio"
+    id="female"
+    name="gender"
+    value="female"
+    hidden
+  />
+  <label htmlFor="female"></label>
+</div>
+      </div>
+      </div>
+
+      <button type="submit" className="submit_btn">
+        Submit
+      </button>
+    </form>
+    
+    
+    </>
+  )
 }
